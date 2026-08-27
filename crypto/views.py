3a021ef1e358d9b9ead1004b66d431403b787337
sergeakaemov <asergey457@gmail.com>
2026-08-27 15:55:09 +1000

    feat(api): add snapshot viewset with separate detail serializer

--- crypto/views.py
+++ crypto/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Snapshot
+from .serializers import SnapshotSerializer, SnapshotDetailSerializer
 
-# Create your views here.
+
+class SnapshotViewSet(viewsets.ModelViewSet):
+    queryset = Snapshot.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return SnapshotDetailSerializer
+        return SnapshotSerializer
