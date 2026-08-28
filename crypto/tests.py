a4b884098d844938910c2caa608c2e73cd8aa9cc
sergeakaemov <asergey457@gmail.com>
2026-08-28 11:24:09 +1000

    test(api): cover pagination, symbol filtering and nested prices

--- crypto/tests.py
+++ crypto/tests.py
@@ -1,3 +1,54 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Snapshot, CoinPrice
 
-# Create your tests here.
+
+class SnapShotApiTests(APITestCase):
+    def setUp(self):
+        for _ in range(12):
+            Snapshot.objects.create(source='test')
+
+    def test_list_is_paginated(self):
+        response = self.client.get('/api/snapshots/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 12)
+        self.assertEqual(len(response.data['results']), 10)
+
+
+class CoinApiTests(APITestCase):
+
+    def setUp(self):
+        self.snapshot = Snapshot.objects.create(source='test')
+        CoinPrice.objects.create(
+            snapshot=self.snapshot,
+            name ='Bitcoin',
+            symbol='BTC',
+            price=1,
+            market_cap=1,
+            total_volume=1,
+            )
+
+        CoinPrice.objects.create(
+            snapshot=self.snapshot,
+            name='Ethereum',
+            symbol='ETH',
+            price=1,
+            market_cap=1,
+            total_volume=1,
+            )
+
+    def test_filter_by_symbol(self):
+        response = self.client.get('/api/coins/?symbol=BTC')
+
+        self.assertEqual(response.data['count'], 1)
+
+    def test_filter_is_case_insensitive(self):
+        response = self.client.get('/api/coins/?symbol=btc')
+
+        self.assertEqual(response.data['count'], 1)
+
+    def test_detail_contains_prices(self):
+        response = self.client.get(f'/api/snapshots/{self.snapshot.id}/')
+
+        self.assertEqual(len(response.data['prices']), 2)
+
