551d958aad8b7a36b8cd05b1350199d8e841708b
sergeakaemov <asergey457@gmail.com>
2026-09-17 13:54:32 +1000

    build: add simplejwt and pytest-django, configure exchange provider

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,9 @@ MAILERS = {
     },
 }
 
+# Биржа для запросов цен и проверки символов: ключ из crypto.sources.SOURCES
+EXCHANGE_PROVIDER = os.getenv('EXCHANGE_PROVIDER', 'coingecko')
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
